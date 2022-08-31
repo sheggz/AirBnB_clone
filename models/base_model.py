ceb4e543cbe89e0a38e77d2613ac53e7b8d3358d
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-08-31 18:30:37 +0000

    Task completed!

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,9 @@ import uuid
 from datetime import datetime
 
 
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+
 class BaseModel:
     """Defines the blueprint of the subclasses.
 
@@ -19,11 +22,20 @@ class BaseModel:
                     you change your object
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """An object constructor method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            self.__dict__ = kwargs
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs.get("created_at"),
+                                                    time_format)
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs.get("updated_at"),
+                                                    time_format)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """A string representation of object"""
