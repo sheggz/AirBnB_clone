05dbe2fd8cd313b843792bcb5163a0e8b7607c35
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-09-02 02:03:33 +0000

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 This module contains a class that defines all other classes in this project.
 """
+import models
 import uuid
 from datetime import datetime
 
@@ -24,7 +25,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """An object constructor method"""
-        if len(kwargs) > 0:
+        if kwargs:
             self.__dict__ = kwargs
             if "created_at" in kwargs:
                 self.created_at = datetime.strptime(kwargs.get("created_at"),
@@ -35,7 +36,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """A string representation of object"""
@@ -45,9 +46,10 @@ class BaseModel:
     def save(self):
         """updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """returns all keys/values of __dict__ of the instance"""
+        """Returns all keys/values of __dict__ of the instance"""
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = new_dict["created_at"].isoformat()
