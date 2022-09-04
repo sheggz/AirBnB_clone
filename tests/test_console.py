231f0696a7575fd6dab3d5445251530b46822246
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2022-09-04 18:05:48 +0000

    Task updated

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,29 @@
-#!/bin/usr/env python3
+#!/usr/bin/env python3
+"""Unittest console module.
+
+Test cases for console class and methods documentation and instances.
+"""
+import unittest
+
+import console
+
+
+Console = console.HBNBCommand
+
+
+class TestConsoleDocs(unittest.TestCase):
+    """Class for testing console docs"""
+
+    def test_doc_file(self):
+        """... documentation for the file"""
+        doc = console.__doc__
+        self.assertIsNotNone(doc)
+
+    def test_doc_class(self):
+        """... documentation for the class"""
+        doc = Console.__doc__
+        self.assertIsNotNone(doc)
+
+
+class TestConsole(unittest.TestCase):
+    """Class for testing Console instances"""
